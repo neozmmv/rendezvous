d468265371052ec4609fc4509b5e97d453a8b72c
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-05-29 23:40:08 -0300

    slash validation on hostname

--- client/main.go
+++ client/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net"
+	"net/http"
 
 	"github.com/pion/stun"
 )
 
 func main() {
+	var hostname string
+	var session string
+	fmt.Print("Enter hostname: ")
+	fmt.Scanln(&hostname)
+	fmt.Print("Enter session: ")
+	fmt.Scanln(&session)
+
+	// remove trailing slash if exists
+	if hostname[len(hostname)-1] == '/' {
+		hostname = hostname[:len(hostname)-1]
+	}
+
 	// open a UDP connection on a random port
 	conn, err := net.ListenUDP("udp", &net.UDPAddr{Port: 0})
 	if err != nil {
@@ -41,5 +56,26 @@ func main() {
 
 	fmt.Printf("Public addr: %s:%d\n", xorAddr.IP, xorAddr.Port)
 
+	// post body
+	body := map[string]string{
+		"udp_addr": fmt.Sprintf("%s:%d", xorAddr.IP, xorAddr.Port),
+	}
+
+	bodyJson, err := json.Marshal(body)
+
+	resp, err := http.Post(fmt.Sprintf("%s/session/%s", hostname, session), "application/json", bytes.NewBuffer(bodyJson))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// read response
+	var respBody map[string]string
+	err = json.NewDecoder(resp.Body).Decode(&respBody)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Printf("Listening on %s\n", conn.LocalAddr().String())
+	fmt.Printf("Peer address: %v", respBody["peer"])
 }
