af46dae5aa24647feb3904800f4a4635325f2c38
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-05-29 21:38:14 -0300

    session route

--- main.go
+++ main.go
@@ -6,8 +6,50 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type SessionInfo struct {
+	UdpAddr string `json:"udp_addr"`
+}
+
+type Peer struct {
+	IP string `json:"ip"`
+}
+type Session struct {
+	peers [2]Peer
+}
+
 func main() {
 	r := gin.Default()
+
+	sessions := map[string]Session{}
+
+	r.POST("/session/:id", func(c *gin.Context) {
+		var sessionInfo SessionInfo
+		sessionId := c.Param("id")
+		fmt.Println("Creating session with id: ", sessionId)
+
+		// server receives udp_addr from client (get by stun server)
+
+		c.BindJSON(&sessionInfo)
+		session, exists := sessions[sessionId]
+		if !exists {
+			// [0]
+			sessions[sessionId] = Session{
+				peers: [2]Peer{
+					{IP: sessionInfo.UdpAddr},
+				},
+			}
+		} else {
+			// [1]
+			session.peers[1] = Peer{IP: sessionInfo.UdpAddr}
+			sessions[sessionId] = session
+		}
+		fmt.Printf("Received session info: %+v\n", sessionInfo)
+		c.JSON(200, gin.H{
+			"message": "Session created",
+			"id":      sessionId,
+			"peers":   sessions[sessionId].peers,
+		})
+	})
 
 	r.GET("/", func(c *gin.Context) {
 		clientIP := c.ClientIP()
